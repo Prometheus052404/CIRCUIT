f71beaf590006226d7eaf5c3e23291d0d4e91b6c
Harith Yerragolam <yharith16@gmail.com>
2024-12-07 13:07:38 +0530

    Formatted Code

--- src/Wire.cpp
+++ src/Wire.cpp
@@ -1,14 +1,14 @@
 #include "../include/Wire.hpp"
 #include "../include/IC.hpp"
 
-Wire::Wire(IC* srcIC, int srcPin, IC* destIC, int destPin)
+Wire :: Wire(IC* srcIC, int srcPin, IC* destIC, int destPin)
     : sourceIC(srcIC), sourcePin(srcPin), destinationIC(destIC), destinationPin(destPin) {}
 
-Wire::~Wire() {
+Wire :: ~Wire() {
     disconnect();
 }
 
-void Wire::connect() {
+void Wire :: connect() {
     if (!sourceIC || !destinationIC) {
         throw runtime_error("Invalid IC connection.");
     }
@@ -17,7 +17,7 @@ void Wire::connect() {
          << " to pin " << destinationPin << " of IC " << destinationIC->getName() << endl;
 }
 
-void Wire::disconnect() {
+void Wire :: disconnect() {
     if (destinationIC) {
         destinationIC->setPin(destinationPin, 0); // Reset the pin
         cout << "Disconnected pin " << destinationPin << " of IC " << destinationIC->getName() << endl;
