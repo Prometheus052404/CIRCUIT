165aeaf5a551bec50148c57c7ac628019e7bda1d
Harith Yerragolam <yharith16@gmail.com>
2024-12-11 19:43:54 +0530

    Cases 5,6,7 added in Main file
    The cases somehow seem to have been erased by mistake. I have added them back in the main file.
    Closes #28

--- main.cpp
+++ main.cpp
@@ -270,6 +270,83 @@ int main() {
                 break;
             }
 
+            case 5: { // Simulate IC
+                if (icList.empty()) {
+                    cout << "No ICs available to simulate.\n";
+                    break;
+                }
+
+                size_t icIndex;
+                cout << "ICs available: \n";
+                for (size_t i = 0; i < icList.size(); ++i) {
+                    cout << i + 1 << ". " << icList[i]->getName() << endl;
+                }
+
+                cout << "Select IC index to simulate (1-" << icList.size() << "): ";
+                cin >> icIndex;
+                if (icIndex < 1 || icIndex > icList.size()) {
+                    cout << "Invalid IC index.\n";
+                    break;
+                }
+
+                try {
+                    icList[icIndex - 1]->simulate();
+                    cout << "IC simulation completed successfully.\n";
+                } catch (const exception& e) {
+                    cout << "Error: " << e.what() << endl;
+                }
+                break;
+            }
+
+            case 6: { // Connect Power and Ground
+                if (icList.empty()) {
+                    cout << "No ICs available to connect power and ground.\n";
+                    break;
+                }
+
+                size_t icIndex;
+                cout << "ICs available: \n";
+                for (size_t i = 0; i < icList.size(); ++i) {
+                    cout << i + 1 << ". " << icList[i]->getName() << endl;
+                }
+
+                cout << "Select IC index to connect power and ground (1-" << icList.size() << "): ";
+                cin >> icIndex;
+                if (icIndex < 1 || icIndex > icList.size()) {
+                    cout << "Invalid IC index.\n";
+                    break;
+                }
+
+                try {
+                    icList[icIndex - 1]->connectVCC();
+                    icList[icIndex - 1]->connectGround();
+                    cout << "Power and ground connected to IC.\n";
+                } catch (const exception& e) {
+                    cout << "Error: " << e.what() << endl;
+                }
+                break;
+            }
+
+            case 7: { // View IC pin states
+                if (icList.empty()) {
+                    cout << "No ICs available to view.\n";
+                    break;
+                }
+
+                for (size_t i = 0; i < icList.size(); ++i) {
+                    cout << "IC " << i + 1 << " (" << icList[i]->getName() << "): ";
+                    for (int pin = 1; pin <= icList[i]->getTotalPins(); ++pin) {
+                        try {
+                            cout << "Pin " << pin << ": " << icList[i]->getPin(pin) << " ";
+                        } catch (const exception&) {
+                            cout << "N/A ";
+                        }
+                    }
+                    cout << endl;
+                }
+                break;
+            }
+
             case 8: // Exit
                 cout << "Exiting program...\n";
                 cleanup(icList, wireList);
